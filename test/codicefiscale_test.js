0cb15ddd5b3ca453bae2c2887e59e762e25b6cfb
parroit <andrea.parodi@ebansoftware.net>
2013-12-04 22:26:58 +0100

    completed

--- test/codicefiscale_test.js
+++ test/codicefiscale_test.js
@@ -6,6 +6,22 @@ var codicefiscale = require("../lib/codicefiscale");
 
 describe("codicefiscale", function () {
     it("is defined", function () {
-        expect(codicefiscale).to.be.an('object');
+        expect(codicefiscale).to.be.an('function');
+    });
+
+    it("check length", function () {
+        expect(codicefiscale("DFFF")).to.be.equal('La lunghezza del codice fiscale non è corretta.');
+    });
+
+    it("check characters", function () {
+        expect(codicefiscale("PRDNDR76A03D969:")).to.be.equal('Il codice fiscale contiene un carattere non valido: \':\'');
+    });
+
+    it("check code", function () {
+        expect(codicefiscale("PRDNDR76A03D969R")).to.be.equal("Il codice fiscale non è corretto:il codice di controllo non corrisponde.");
+    });
+
+    it("return null on right code", function () {
+        expect(codicefiscale("PRDNDR76A03D969Q")).to.be.equal(null);
     });
 });
